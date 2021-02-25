a3d29c34393ba2ad3e51da155e1535706712faf8
Gabriel <yankeegabi.torres@gmail.com>
2021-02-25 14:24:02 -0800

    new stuff

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-import flask
+from flask import Flask;
+from flask import render_template, url_for;
+
+app = Flask(__name__);
+
+@app.route("/")
+@app.route('/home')
+def home():
+    return render_template("home.html", title="home");
