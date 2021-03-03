6f1f59869d82312185a77b010a6058f15e5fc662
Gabriel <yankeegabi.torres@gmail.com>
2021-03-03 14:26:13 -0800

    drop down doesn't work for now

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__);
 def home():
     return render_template("home.html", title="home");
 
+@app.route("/login")
+def login():
+    return render_template("login.html");
+
 if __name__ == "__main__":
     app.run(debug=True)
