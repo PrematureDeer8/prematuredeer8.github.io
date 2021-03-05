5c90575653690c4bbc4a43ef5ba15882bd1785a9
Gabriel <yankeegabi.torres@gmail.com>
2021-03-05 00:11:31 -0800

    new register page

--- app.py
+++ app.py
@@ -12,5 +12,9 @@ def home():
 def login():
     return render_template("login.html");
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
