9fa89613d2ac8b33132f77f79f51d85d6baad4e6
Gabriel <yankeegabi.torres@gmail.com>
2021-03-08 18:52:32 -0800

    new changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ class User(db.Model):
     password = db.Column(db.String(120), nullable=False);
     
     def __repr__(self):
-        return f"User ('{self.username}', '{self.email}', '{self.image_file}')";
+        return f"User ('{self.username}', '{self.email}')";
 
 @app.route("/")
 @app.route('/home')
