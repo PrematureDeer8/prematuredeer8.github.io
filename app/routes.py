6ca4568d4e1c25bbe44b316a5075f6d9048e6b0e
Gabriel <yankeegabi.torres@gmail.com>
2021-03-24 20:45:52 -0700

    fixed gitignore added to requirements

--- app/routes.py
+++ app/routes.py
@@ -44,7 +44,7 @@ def login():
 @app.route('/register', methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('home'))
+        return redirect(url_for('home'));
     form = RegistrationForm();
     if(form.validate_on_submit()): 
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8');
